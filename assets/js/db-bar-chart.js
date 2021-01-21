79d9d0b589cbfed0a8e2ab162cc3c71ed0319114
hoolyuuh <61399425+hoolyuuh@users.noreply.github.com>
2021-01-22 03:35:13 +0800

    dashboard views and initial script

---
+++ assets/js/db-bar-chart.js
@@ -0,0 +1,51 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+
+// Bar Chart Example
+var ctx = document.getElementById("dbRevAndPurch");
+var myLineChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ["January", "February", "March", "April", "May", "June"],
+    datasets: [{
+      label: "Revenue",
+      backgroundColor: "green",
+      borderColor: "green",
+      data: [4215, 5312, 6251, 7841, 9821, 14984],
+    }, {
+      label: "Purchases",
+      backgroundColor: "red",
+      borderColor: "red",
+      data: [4215, 5312, 6251, 7841, 9821, 14984],
+    }, ],
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'month'
+        },
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 6
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 15000,
+          maxTicksLimit: 5
+        },
+        gridLines: {
+          display: true
+        }
+      }],
+    },
+    legend: {
+      display: true
+    }
+  }
+});
