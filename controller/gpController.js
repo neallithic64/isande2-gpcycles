a9f89b4d185f16ca3b74976c732ef1d450a42d82
Neal Lim <55176465+neallithic64@users.noreply.github.com>
2021-01-21 11:24:23 +0800

    imported models to controller

--- controller/gpController.js
+++ controller/gpController.js
@@ -1,5 +1,10 @@
 const db = require('../models/db');
-// model imports
+const User = require('../models/User');
+const Supplier = require('../models/Supplier');
+const Customer = require('../models/Customer');
+const SalesOrder = require('../models/SalesOrder');
+const PurchaseOrder = require('../models/PurchaseOrder');
+const Product = require('../models/Product');
 
 /* Index Functions
  */
