dc13674f43b960dd2bb9906d032ee9f080e058d1
Neal Lim <55176465+neallithic64@users.noreply.github.com>
2021-01-23 10:03:26 +0800

    starting post requests with login and register

--- controller/gpController.js
+++ controller/gpController.js
@@ -6,6 +6,9 @@ const SalesOrder = require('../models/SalesOrder');
 const PurchaseOrder = require('../models/PurchaseOrder');
 const Product = require('../models/Product');
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 function forceJSON(e) {
 	return JSON.parse(JSON.stringify(e));
 }
@@ -26,7 +29,19 @@ const gpController = {
 	
 	
 	postLogin: async function(req, res) {
-		
+		let {username, password} = req.body;
+		try {
+			let user = await db.findOne(User, {username: username});
+			req.session.user = user;
+			res.status(200).send();
+		} catch (e) {
+			res.status(500).send('Server error.');
+		}
+	},
+	
+	postRegister: async function(req, res) {
+		let {username, password, usertype, name} = req.body;
+		// submitting here to db
 	}
 };
 
