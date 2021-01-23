89bf2dd356b8e87ea0bd5b118286e6afd89b2cb5
Neal Lim <55176465+neallithic64@users.noreply.github.com>
2021-01-23 11:35:34 +0800

    initial routes

--- router/gpRouter.js
+++ router/gpRouter.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express();
 const test = require('../controller/indexTest');
+const gpController = require('../controller/gpController');
+const gpMiddleware = require('../middlewares/gpMiddleware');
 
 // Testing Routes; SANDBOXING PURPOSES ONLY
 router.get('/test', test.getHome);
+
+// GET Routes
+router.get('/login', gpController.getLogin);
+
+// POST Routes
+router.post('/login', gpMiddleware.validateLogin, gpController.postLogin);
+router.post('/register', gpMiddleware.validateRegister, gpController.postRegister);
 
 // Error Page
 router.get('*', function(req, res) {
