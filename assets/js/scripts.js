62638ef860ff96fc43733a68eab22c09ed444efa
Neal Lim <55176465+neallithic64@users.noreply.github.com>
2021-01-26 14:09:40 +0800

    added validatorjs

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,6 +3,8 @@
 * Copyright 2013-2020 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
 */
+/* global validator */
+
 (function($) {
 	"use strict";
 	// Add active state to sidbar nav links
