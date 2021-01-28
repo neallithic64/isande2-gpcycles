c5399f2e27b97830740338b54220469dc5a1aa0c
hoolyuuh <61399425+hoolyuuh@users.noreply.github.com>
2021-01-29 03:09:43 +0800

    view all <thing>, confirm so & po, new partials

---
+++ assets/js/viewusercustsupp-datatables.js
@@ -0,0 +1,12 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+    $('#viewCustTable').DataTable();
+  });
+
+$(document).ready(function() {
+    $('#viewSuppTable').DataTable();
+  });
+
+$(document).ready(function() {
+    $('#viewUsersTable').DataTable();
+  });
