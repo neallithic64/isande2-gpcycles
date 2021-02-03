14aac85d01105805db5b7ac142971896ae3a55e7
Neal Lim <55176465+neallithic64@users.noreply.github.com>
2021-02-03 11:08:47 +0800

    scrubbing scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,8 +5,6 @@
 */
 /* global validator */
 
-let isFilter = false;
-
 (function($) {
 	"use strict";
 	// Add active state to sidbar nav links
@@ -494,11 +492,13 @@ $(document).ready(function() {
 			}
 		});
 	});
-	
-	
-	
-	// STATUS UPDATE FUNCTIONS
-	
+});
+
+
+// STATUS UPDATE FUNCTIONS
+
+
+$(document).ready(function() {
 	$("#cancelSOPOSubmitButton").click(function() {
 		let ordNum = window.location.pathname.split('/')[2], reason = $("#inputCancelRemarks").val();
 		$.ajax({
@@ -514,7 +514,7 @@ $(document).ready(function() {
 			}
 		});
 	});
-	
+
 	$("#paySOPOSubmitButton").click(function() {
 		let ordNum = window.location.pathname.split('/')[2],
 			penalty = $("#inputPenalty").val(),
@@ -532,7 +532,7 @@ $(document).ready(function() {
 			}
 		});
 	});
-	
+
 	$("#submitDeliverSOButton").click(function() {
 		let urlParams = new URLSearchParams(window.location.search);
 		let partial = urlParams.get('partial') === "true", partialList = [];
@@ -560,7 +560,7 @@ $(document).ready(function() {
 			}
 		});
 	});
-	
+
 	$("#submitReceivePOButton").click(function() {
 		let urlParams = new URLSearchParams(window.location.search);
 		let partial = urlParams.get('partial') === "true", partialList = [];
@@ -589,6 +589,10 @@ $(document).ready(function() {
 		});
 	});
 });
+
+
+// EDITING PRODUCT FUNCTIONS
+
 
 $(document).ready(function() {
 
@@ -633,7 +637,7 @@ $(document).ready(function() {
 				url: "/editproduct/" + window.location.pathname.split('/')[2],
 				data: editProductForm,
 				success: function() {
-					window.location.href = "/viewproduct/" + window.location.pathname.split('/')[2]; 
+					window.location.href = "/viewproduct/" + window.location.pathname.split('/')[2];
 				},
 				error: function(str) {
 					alert(str.responseText);
@@ -672,6 +676,8 @@ $(document).ready(function() {
 
 });
 
+var isFilter = false;
+
 function filter() {
 	console.log(isFilter);
 	if (isFilter) {
@@ -689,7 +695,7 @@ function filter() {
 	isFilter = !isFilter;
 	console.log(isFilter);
 };
-    
+
 function logout() {
 	let xhr = new XMLHttpRequest();
 	xhr.open('POST', '/logout');
@@ -743,7 +749,6 @@ var grp = 0;
 $(document).ready(function() {
 	// Call the dataTables jQuery plugin
 	$('#datatable').dataTable();
-	
 	$('#report').dataTable({
 		'iDisplayLength': 100
 	});
@@ -751,74 +756,72 @@ $(document).ready(function() {
 	$("#myInput").on("keyup", function() {
 		var value = $(this).val().toLowerCase();
 		$(".dropdown-menu li").filter(function() {
-		  $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+			$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
 		});
 	});
 
 	if (window.location.pathname === "/inventoryreport") {
 		let ulElem = $("#groupnum");
-		let str = "00: Adaptor\n01: Aero Bar\n02: Bar Tape\n04: Bell\n05: Bike Bag\n06: Bike Light\n07: Bike Racks\n08: Bottle\n09: Bottle Cage\n10: Bottom Bracket\n11: Brakes\n12: Cable\n13: Cage Storage\n14: Caliper Road\n15: Chain\n16: Chain Catcher\n17: Chain Guide\n18: Crankset\n19: Drop Out\n21: Frame\n22: Free Hub\n23: Front Derailleur\n24: Group Set\n25: Handle Bar\n26: Handle Grip\n27: Headset\n28: Helmet\n29: Hub\n30: Lube\n33: Others\n34: Patch\n35: Pedal\n36: Pulley\n37: Pump\n39: Rebuild Kit\n40: STI\n41: Sealant\n42: Seat Clamp\n43: Seat Post\n45: Shoe Cover\n46: Skewer\n47: Spacer\n48: Speedometer\n49: Sprocket\n51: Tire\n52: Tools\n53: Tube\n54: Tubeless Valve\n55: Unit\n56: Valve Extender";
+		let str = "00: Adaptor\n01: Aero Bar\n02: Bar Tape\n04: Bell\n05: Bike Bag\n" +
+				"06: Bike Light\n07: Bike Racks\n08: Bottle\n09: Bottle Cage\n10: B" +
+				"ottom Bracket\n11: Brakes\n12: Cable\n13: Cage Storage\n14: Calipe" +
+				"r Road\n15: Chain\n16: Chain Catcher\n17: Chain Guide\n18: Crankse" +
+				"t\n19: Drop Out\n21: Frame\n22: Free Hub\n23: Front Derailleur\n24" +
+				": Group Set\n25: Handle Bar\n26: Handle Grip\n27: Headset\n28: Hel" +
+				"met\n29: Hub\n30: Lube\n33: Others\n34: Patch\n35: Pedal\n36: Pull" +
+				"ey\n37: Pump\n39: Rebuild Kit\n40: STI\n41: Sealant\n42: Seat Clam" +
+				"p\n43: Seat Post\n45: Shoe Cover\n46: Skewer\n47: Spacer\n48: Spee" +
+				"dometer\n49: Sprocket\n51: Tire\n52: Tools\n53: Tube\n54: Tubeless" +
+				" Valve\n55: Unit\n56: Valve Extender";
 		str.split("\n").forEach(e => ulElem.append("<li onclick='group(\"" + e.split(": ")[0] + "\")'>" + e + "</li>"));
 	}
-
 });
 
 function group(grp) {
-	$('tbody tr').each(function(index,element) {
-		element.style.display="";
-		if(grp!=="-1" && !element.classList.contains('g'+grp))
-			element.style.display="none";
-	})
+	$('tbody tr').each(function(i, element) {
+		element.style.display = "";
+		if(grp !== "-1" && !element.classList.contains('g' + grp))
+			element.style.display = "none";
+	});
 }
 
 function downloadCSV(csv, filename) {
-    var csvFile;
-    var downloadLink;
-
-    // CSV file
-    csvFile = new Blob([csv], {type: "text/csv"});
-
-    // Download link
-    downloadLink = document.createElement("a");
-
-    // File name
-    downloadLink.download = filename;
-
-    // Create a link to the file
-    downloadLink.href = window.URL.createObjectURL(csvFile);
-
-    // Hide download link
-    downloadLink.style.display = "none";
-
-    // Add the link to DOM
-    document.body.appendChild(downloadLink);
-
-    // Click download link
-    downloadLink.click();
+	var csvFile;
+	var downloadLink;
+	// CSV file
+	csvFile = new Blob([csv], {type: "text/csv"});
+	// Download link
+	downloadLink = document.createElement("a");
+	// File name
+	downloadLink.download = filename;
+	// Create a link to the file
+	downloadLink.href = window.URL.createObjectURL(csvFile);
+	// Hide download link
+	downloadLink.style.display = "none";
+	// Add the link to DOM
+	document.body.appendChild(downloadLink);
+	// Click download link
+	downloadLink.click();
 }
 
 function exportTableToCSV(filename) {
-    var csv = [];
-    var rows = document.querySelectorAll("table tr");
-    
-    for (var i = 1; i < rows.length; i++) {
-        var row = [], cols = rows[i].querySelectorAll("td, th");
-        
-        for (var j = 0; j < cols.length; j++) 
-            row.push(cols[j].innerText);
-        
-        csv.push(row.join(","));        
-    }
-
-    // Download CSV file
-    downloadCSV(csv.join("\n"), filename);
+	var csv = [];
+	var rows = document.querySelectorAll("table tr");
+	for (var i = 1; i < rows.length; i++) {
+		var row = [], cols = rows[i].querySelectorAll("td, th");
+		for (var j = 0; j < cols.length; j++) 
+			row.push(cols[j].innerText);
+		csv.push(row.join(","));
+	}
+	// Download CSV file
+	downloadCSV(csv.join("\n"), filename);
 }
 
-//  function saveDiv(divId, title) {
-// 	var doc = new jsPDF();
-//  doc.fromHTML(`<html><head><title>${title}</title></head><body>` + document.getElementById(divId).innerHTML + `</body></html>`);
-//  doc.save('div.pdf');
-// }
+//function saveDiv(divId, title) {
+//var doc = new jsPDF();
+//doc.fromHTML(`<html><head><title>${title}</title></head><body>` + document.getElementById(divId).innerHTML + `</body></html>`);
+//doc.save('div.pdf');
+//}
 
 function printDiv(div) {
 	var printContents = document.getElementById(div).innerHTML;
@@ -827,4 +830,3 @@ function printDiv(div) {
 	window.print();
 	document.body.innerHTML = original;
 }
-
